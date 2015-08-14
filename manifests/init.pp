4d3354fc6426f298eda290e6ebbeded537525216
Guillaume VAILLANT <guillaume.vaillant@rentabiliweb.com>
2015-08-14 10:32:21 +0200

    added a special treatment for Debian based system (based on preseeding instead of stdlib/file_line)

--- manifests/init.pp
+++ manifests/init.pp
@@ -17,28 +17,64 @@ class snoopy ($package = true, $service = true) {
     default : { fail('package must be true, false or lastest') }
   }
 
-  case $service {
-    true    : { $ensure_service = 'present' }
-    false   : { $ensure_service = 'absent' }
-    default : { fail('service must be true or false') }
-  }
-
-  package { $snoopy_package_name: ensure => $ensure_package, }
-
-  if $service == true {
-    file { '/etc/ld.so.preload':
-      ensure => $ensure_service,
-      path   => '/etc/ld.so.preload',
-      owner  => 'root',
-      group  => 'root',
-      mode   => '0644',
+  if $::operatingsystem =~ /(Debian|Ubuntu)/ {
+    case $service {
+      true    : {
+        $ensure_service = 'true'
+      }
+      false   : {
+        $ensure_service = 'false'
+      }
+      default : { fail('service must be true or false') }
     }
-
-    file_line { 'snoopy':
-      ensure  => $ensure_service,
-      line    => $snoopy_lib_path,
-      path    => '/etc/ld.so.preload',
-      require => File['/etc/ld.so.preload'],
+  
+    if $service and !$package {
+        fail("Can't have service without package!")
+    }
+  
+    package { $snoopy_package_name:
+      alias                 => 'snoopy',
+      ensure                => $ensure_package,
+      responsefile          => '/var/local/debconf.snoopy.preseed',
+      require               => File['debconf.snoopy.preseed'],
+    }
+  
+    file { 'debconf.snoopy.preseed':
+      path    => '/var/local/debconf.snoopy.preseed',
+      ensure  => present,
+      mode    => '0400',
+      owner   => 'root',
+      content =>  template('snoopy/debconf.snoopy.preseed.erb'),
+    }
+  
+    # à affiner pour que ça marche hors galaxie Debian
+    exec { 'reconfigure':
+      command => "/usr/bin/debconf-set-selections /var/local/debconf.snoopy.preseed; /usr/sbin/dpkg-reconfigure -fnoninteractive $snoopy_package_name",
+      refreshonly => true,
+      subscribe => File['debconf.snoopy.preseed'],
+      require => Package['snoopy'],
+    }
+  } else {
+    case $service {
+      true    : { $ensure_service = 'present' }
+      false   : { $ensure_service = 'absent' }
+      default : { fail('service must be true or false') }
+    }
+    package { $snoopy_package_name: ensure => $ensure_package, }
+    if $service == true {
+      file { '/etc/ld.so.preload':
+        ensure => $ensure_service,
+        path   => '/etc/ld.so.preload',
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0644',
+      }
+      file_line { 'snoopy':
+        ensure  => $ensure_service,
+        line    => $snoopy_lib_path,
+        path    => '/etc/ld.so.preload',
+        require => File['/etc/ld.so.preload'],
+      }
     }
   }
 }
